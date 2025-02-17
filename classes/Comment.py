58128029572425a5843b8800a5ad171c2c1901bd
Tehila <tzvulunov@gmail.com>
2025-02-17 19:29:31 +0200

    changes lesson 17.02

--- classes/Comment.py
+++ classes/Comment.py
@@ -1,3 +1,15 @@
+import pygame
+
+from constants import *
+from helpers import *
+from Post import Post
+
+# doneeeeee :)
 class Comment:
-    def __init__(self):
-        pass
+    def __init__(self, text):
+        self.text = text
+
+    def display(self, comment_index):
+        font = pygame.font.SysFont('chalkduster.ttf', COMMENT_TEXT_SIZE)
+        text = font.render(self.text, True, BLACK)
+        screen.blit(text, (FIRST_COMMENT_X_POS, FIRST_COMMENT_Y_POS+comment_index*COMMENT_LINE_HEIGHT))
--- classes/TextPost.py
+++ classes/TextPost.py
@@ -1,16 +1,23 @@
 import pygame
 
 from constants import *
-from helpers import screen
+from helpers import *
 from Post import Post
 
-
+# done :)
 class TextPost(Post):
-    def __init__(self,text, text_color, background_color):
-        self.text= text
-        self.text_color=text_color
-        self.background_color= background_color
 
-    def display(self):
-        background = pygame.Rect(POST_WIDTH, POST_HEIGHT, POST_X_POS, POST_Y_POS)
+    def __init__(self, username, location, description, text, text_color, background_color):
+        super().__init__(username, location, description)
+        self.text = from_text_to_array(text) # list
+        self.text_color = text_color
+        self.background_color = background_color
+
+    def display_content(self):
+        background = pygame.Rect(POST_X_POS, POST_Y_POS, POST_WIDTH, POST_HEIGHT)
         pygame.draw.rect(screen, self.background_color, background)
+        # go over the list and display all the text
+        for i in range (0, len(self.text)):
+            font = pygame.font.SysFont('chalkduster.ttf', TEXT_POST_FONT_SIZE)
+            text = font.render(self.text[i], True, self.text_color)
+            screen.bilt(text,center_text(len(self.text), text, i))
